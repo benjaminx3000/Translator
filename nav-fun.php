342287ada8911fda343e79549b36bbb918f9fcf6
Benjamin Liesch <benjamin.david.design@gmail.com>
2012-07-17 16:29:56 -0500

    worked on main nav. nearly complete need animation for flask :)

---
+++ nav-fun.php
@@ -0,0 +1,20 @@
+	<div id="fun-nav">
+		<div id="folio">
+			
+			<div class="sprite fun-nav_folio-back"></div>
+			<div class="sprite fun-nav_folio-front"><h3>CLIENT</h3></div>
+			<div class="folio-bottom"></div>
+		</div><!-- end folio -->
+
+		<div id="flask">
+			<h3>LAB</h3>
+			<div class="sprite fun-nav_flask"></div>
+			<div class="sprite fun-nav_bubble"></div>
+		</div><!-- end flask -->
+
+		<div id="lamp">
+			<h3>STUDIO</h3>
+			<div class="sprite fun-nav_light-ray"></div>
+			<div class="sprite fun-nav_lamp"></div>
+		</div><!-- end lamp -->
+	</div>
