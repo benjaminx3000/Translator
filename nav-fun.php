58e077319d2709eed2ec9789b8f6c459827bf96f
Benjamin Liesch <benjamin.david.design@gmail.com>
2012-08-03 15:44:28 -0500

    added links to the FUN nav

--- nav-fun.php
+++ nav-fun.php
@@ -1,20 +1,20 @@
 	<div id="fun-nav">
-		<div id="folio">
+		<a href="<?php echo site_url('/client/') ?>" id="folio">
 			
 			<div class="sprite fun-nav_folio-back"></div>
 			<div class="sprite fun-nav_folio-front"><h3>CLIENT</h3></div>
 			<div class="folio-bottom"></div>
-		</div><!-- end folio -->
+		</a><!-- end folio -->
 
-		<div id="flask">
+		<a href="<?php echo site_url('/lab/') ?>" id="flask">
 			<h3>LAB</h3>
 			<div class="sprite fun-nav_flask"></div>
 			<!-- <div class="wrapper"><div class="sprite fun-nav_bubble"></div></div> -->
-		</div><!-- end flask -->
+		</a><!-- end flask -->
 
-		<div id="lamp">
+		<a href="<?php echo site_url('/studio/') ?>" id="lamp">
 			<h3>STUDIO</h3>
 			<div class="sprite fun-nav_light-ray"></div>
 			<div class="sprite fun-nav_lamp"></div>
-		</div><!-- end lamp -->
+		</a><!-- end lamp -->
 	</div>
