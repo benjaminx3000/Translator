1290929adaacaed3c5df75679b52fe0d803cce12
Benjamin Liesch <benjamin.david.design@gmail.com>
2012-10-17 16:35:23 -0500

    tweaks to event modal view and singles

--- single.php
+++ single.php
@@ -6,6 +6,28 @@
 					<div class="container">
 						<?php while ( have_posts() ) : the_post(); ?>
 							<h1 class="page-title"><?php the_title() ?></h1>
+							
+							<?php if(get_post_type() == 't_event') { //if event?>
+								<div class="date-time">
+									<h4 class="date-time">
+										<span class="date">
+											<?php echo get_post_meta(get_the_id(), '_event_date', true); ?>
+										</span><br>
+										<span class="time">
+											<?php echo get_post_meta(get_the_id(), '_event_start_time', true) . ' - ' . get_post_meta(get_the_id(), '_event_end_time', true); ?>
+										<span>
+									</h4>
+									<?php $link =  get_post_meta(get_the_id(), '_location_link', true); ?>
+									<p>
+										<span class="location-name">
+											<?php echo get_post_meta(get_the_id(), '_location', true); ?>
+										</span>
+										<span class="address"><?php echo get_post_meta(get_the_id(), '_address', true); ?></span> 
+										<?php if($link){echo " <a class='location' href='$link' target='_blank'>map</a>";} ?>
+									</p>
+								</div>
+								<?php } //end if event ?>
+
 							<?php the_content(); ?>
 						<?php endwhile; // end of the loop. ?>
 						<div class="comments">
